9ce80295cc0131584870dadd971a025b29b781ec
jeffersonjoseph5538-cmyk <jeffersonjoseph5538@gmail.com>
2026-08-04 17:07:22 -0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // Ces valeurs sont publiques et sans danger à exposer dans le code frontend,
 // car la sécurité réelle est assurée par les policies RLS de Supabase.
 const supabaseUrl = 'https://fyxzxjlldbnftbbhiosm.supabase.co';
-const supabaseKey = 'sb_publishable_v_Ih3PaQd3m3dW9pT0TmqQ_nwWBgm8q';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5eHp4amxsZGJuZnRiYmhpb3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MTkwNzIsImV4cCI6MjEwMTI5NTA3Mn0.jISYA4NHiYLIOz5OQL74bu_Cjdch-Sqtx2AIiHw2ZKw';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
